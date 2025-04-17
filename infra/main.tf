dc8478a987028c8561245369682ff6f54301e9e4
Christian Hinojosa <chrhinojosam@upt.pe>
2025-04-17 14:44:17 -0500

    Quito creación de base de datos, ya existe

--- infra/main.tf
+++ infra/main.tf
@@ -3,11 +3,3 @@ provider "azurerm" {
 
   subscription_id = "715ad6a0-5b95-442f-b928-60cffbfb75c6"
 }
-
-# Solo se creará la base de datos usando el servidor SQL ya existente
-resource "azurerm_mssql_database" "sqldb" {
-  name                = "AdventureWorksLT"
-  server_id           = "/subscriptions/715ad6a0-5b95-442f-b928-60cffbfb75c6/resourceGroups/rg-powerbi-demo/providers/Microsoft.Sql/servers/sqlserverpowerbidemoxyz"
-  collation           = "SQL_Latin1_General_CP1_CI_AS"
-  sku_name            = "Basic"
-}
