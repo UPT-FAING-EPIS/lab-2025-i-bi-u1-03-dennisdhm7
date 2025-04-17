6da3b75f7a52a7a70875f2a86474ffa4b3021aa5
Christian Hinojosa <chrhinojosam@upt.pe>
2025-04-17 14:40:22 -0500

    Uso servidor SQL existente, solo creo base de datos

--- infra/main.tf
+++ infra/main.tf
@@ -4,20 +4,10 @@ provider "azurerm" {
   subscription_id = "715ad6a0-5b95-442f-b928-60cffbfb75c6"
 }
 
-resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "sqlserverpowerbidemoxyz"
-  resource_group_name          = "rg-powerbi-demo"       # Valor fijo
-  location                     = "westus"                # Valor fijo
-  version                      = "12.0"
-  administrator_login          = "adminuser"
-  administrator_login_password = "Upt2025@"
-  minimum_tls_version          = "1.2"
-  public_network_access_enabled = true
-}
-
+# Solo se creará la base de datos usando el servidor SQL ya existente
 resource "azurerm_mssql_database" "sqldb" {
   name                = "AdventureWorksLT"
-  server_id           = azurerm_mssql_server.sqlserver.id
+  server_id           = "/subscriptions/715ad6a0-5b95-442f-b928-60cffbfb75c6/resourceGroups/rg-powerbi-demo/providers/Microsoft.Sql/servers/sqlserverpowerbidemoxyz"
   collation           = "SQL_Latin1_General_CP1_CI_AS"
   sku_name            = "Basic"
 }
