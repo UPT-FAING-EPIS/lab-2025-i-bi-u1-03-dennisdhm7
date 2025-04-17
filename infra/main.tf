5c8b7b016f1d3d2bcb7457a8e049eae98c34db8a
Christian Hinojosa <chrhinojosam@upt.pe>
2025-04-17 14:26:16 -0500

    Quito creación de resource group ya existente

--- infra/main.tf
+++ infra/main.tf
@@ -4,10 +4,6 @@ provider "azurerm" {
   subscription_id = "715ad6a0-5b95-442f-b928-60cffbfb75c6"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-powerbi-demo"
-  location = "westus"
-}
 
 resource "azurerm_mssql_server" "sqlserver" {
   name                         = "sqlserverpowerbidemoxyz"
